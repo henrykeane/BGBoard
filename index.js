2589db8f8777ae1571c35dc3b621a84e1d979715
HENRY\Henry <hkeane@usc.edu>
2019-11-07 13:39:21 -0500

    BGBoard 1.0 complete
    Checks for "localchannel", must manually insert that field, as well as which channel the bot is active.
    Otherwise, I'm satisfied with this MVP.

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const topNum = 3;				//How many people do we care about
+const spamTimer = 5000;			//How often do we limit chat
+const refreshTimer = 240000;	//How often do we recheck the leaderboard
 const tmi = require('tmi.js')
+const Nightmare = require('nightmare');
+const cheerio = require('cheerio');
+const URL = 'https://playhearthstone.com/en-us/community/leaderboards?region=US&leaderboardId=BG';
+
+//We don't want to spam chat, so we keep track so we only send messages once every 5 seconds
+var lastCommandResponded = 0;
+var lastData = undefined;
+
+const playerMapping = {
+	tidesoftime:"tidesoftime"
+}
 
 //options
 const opts = {
@@ -10,26 +24,14 @@ const opts = {
         'neesh245'
     ]
 };
-
-// Create a client with our options
 const client = new tmi.client(opts);
 
 // Register our event handlers (defined below)
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 client.on('close',onCloseHandler)
-
-function onCloseHandler(){
-    console.info("Reconnecting service");
-    client.connect();
-}
-// Connect to Twitch:
 client.connect();
 
-//This is set up such that you can't call upon the bot more than once every 5 seconds
-var lastCommandResponded = Date.now()-5000;
-
-// Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
@@ -37,17 +39,21 @@ function onMessageHandler (target, context, msg, self) {
   const commandName = msg.trim().toLowerCase();
 
   // bgleaderboard
-  if (commandName === '!test') {
+  if (commandName === '!bgrank') {
     if(Date.now() >= lastCommandResponded+5000){
+		if(Date.now() >= lastCommandResponded+refreshTimer){
+	        scrapeLeaderboard(target)
+	        .then(response=>{
+	            console.log(response);
+	            client.say(target,response);
+	        });
+		}else{
+			console.log("past data",lastData)
+			client.say(target,lastData);
+		}
         lastCommandResponded = Date.now();
-        scrapeLeaderboard(client,target)
-        .then(response=>{
-            var printMe = "Success: "+response;
-            console.log(printMe);
-            client.say(target,printMe);
-        });
     }else{
-        console.log("Command recognized, rate limiting");
+        console.info("Command recognized, rate limiting");
     }
   }
 }
@@ -57,63 +63,64 @@ function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
 
-const Nightmare = require('nightmare');
-const cheerio = require('cheerio');
-const URL = 'https://playhearthstone.com/en-us/community/leaderboards?region=US&leaderboardId=BG';
-// const URL = 'https://google.com/'
-function scrapeLeaderboard(client,target){
+function scrapeLeaderboard(target){
     return new Promise((resolve, reject) => {
-        console.log("attempting nightmare scrape")
+        console.info("Nightmare scraping...")
+        var longasstime = 120000; //My desktop has trash spectrum internet so im setting this to an absurdly long timeout
         const nightmare = Nightmare({
-            waitTimeout: 120000,
-            gotoTimeout: 120000,
-            loadTimeout: 120000
+            waitTimeout: longasstime,
+            gotoTimeout: longasstime,
+            loadTimeout: longasstime
         });
         nightmare.goto(URL)
         .wait('.LeaderboardsTable-Rendered')
-        // .wait('body')
-        .evaluate(()=>document.querySelector('.LeaderboardsTable-Rendered').innerHTML)
+        .evaluate(()=>document.querySelector('#leaderBoardsTable').innerHTML)
         .end()
         .then(response=>{
-            // console.log(getData(response));
             var topLeaderboard = getData(response);
             resolve(topLeaderboard)
         }).catch(err=>{
             console.log(err);
         })
         let getData = html=>{
-            // data = [];
             var data = "";
             const $ = cheerio.load(html);
 
-            console.log($.root());
+            var table = $('.LeaderboardsTable-Rendered');
+            table.children().each(function(_i, elem) {
+            	//Grab each relevant field
+                var rank = $(elem).find('.col-rank').text().trim();
+                var battletag = $(elem).find('.col-battletag').text().trim();
+                var rating = $(elem).find('.col-rating').text().trim();
 
-            $.root().children().each(function(_i, elem) {
-                var rank = $(elem).find('col-rank').text().trim()
-                var accountid = $(elem).find('col-accountid').text().trim()
-                var rating = $(elem).find('col-rating').text().trim()
+                //Check to see if the current channel is a special case
+                //This was created just to make sure Tides is still posted
+                //even if he's rank 4 RareParrot
+                var localChannel = false;
+				if(playerMapping.hasOwnProperty(target.substr(1))){
+					localChannel = true
+				}
 
+				//Finicky message formatting.
+				//2nd case: if localchannel is top 25 but not within topNum
+                if((parseInt(rank) < topNum+1 && battletag) || localChannel){
+					var rankRow = `#${rank}: ${battletag} - ${rating}pts`
+					if(rank < topNum){
+						data += rankRow + " // ";
+					}else if(rank > topNum){
+						data += " // " + rankRow;
+					}else{
+						data += rankRow;
+					}
+                }
             });
-
-            // $.children().each(function(_i, elem) {
-            //     if(elem.hasClass("row")){
-            //         console.log(elem.text())
-            //     }
-            //     // $(elem).find('a').attr('href');
-                
-            //     // var it = $(elem).find('a');
-            //     // var spellLink = it.attr('href');
-        
-            //     // spellLinks.push(delay(spellLink));
-                
-            //     // it = it.parent().next();
-            //     // it = it.next();
-            // });
-
-            data = $.text().substring(0,10);
-            // data.push
+            lastData = data;
             return data;
         }
 
     });
+}
+function onCloseHandler(){
+    console.info("Reconnecting service");
+    client.connect();
 }
